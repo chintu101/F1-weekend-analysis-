ed77d4594e2899950bce7f49e5e2fc659b50d8ba
Sarvesh Sathyanarayanan <sarvesh112170@gmail.com>
2025-06-13 16:56:32 +0530

    update

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 import fastf1
+import matplotlib.pyplot as plt
+import datetime
+
+today = datetime.datetime.now()
+curr_year = today.year
+
+for year in range(curr_year, 2024, -1):
+    schedule = fastf1.get_event_schedule(year=year)
+    
+
+event_name = latest_event['EventName']
+
