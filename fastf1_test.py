dc8dada7acbf8a0f98fa3699dccf549f77057525
Sarvesh Sathyanarayanan <sarvesh112170@gmail.com>
2025-08-09 13:09:28 +0530

    cache is set to my folder
    deciding on what to display for each driver, lets focus on just per driver analysis for now

--- fastf1_test.py
+++ fastf1_test.py
@@ -1,13 +1,17 @@
 import fastf1.plotting
 import fastf1 as F
 import pandas as pd
-
-fastf1.Cache.disabled()
-session = F.get_session(2023,1, "Q")
+fastf1.Cache.enable_cache("D:\github-desktop-reps\F1-weekend-analysis-\cache")
+session = F.get_session(2023, 1, "R")
 session.load()
 
 driver = session.get_driver('VER')
-print(driver)
+driver_laps = session.laps.pick_drivers('VER')
+
+print(driver['DriverNumber'])  #driver number
+print(driver_laps['LapTime'])  #driver total laps
+print(driver_laps['LapTime'].min()) #fastest lap of the session
+
 
 
 
