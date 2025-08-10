44585793dd0d7b5588175732ee4745dd5dc453e2
Sarvesh Sathyanarayanan <sarvesh112170@gmail.com>
2025-08-10 08:39:29 +0530

    finally got the timedelta converted to mm:ss:ms

--- fastf1_test.py
+++ fastf1_test.py
@@ -1,6 +1,7 @@
 import fastf1.plotting
 import fastf1 as F
 import pandas as pd
+from datetime import timedelta
 
 
 fastf1.Cache.enable_cache("D:\github-desktop-reps\F1-weekend-analysis-\cache")
@@ -8,16 +9,27 @@ session = F.get_session(2023, 1, "R")
 session.load()
 pd.options.display.max_columns = None
 
+def format_time_delta(td):
+    total_ms = td.total_seconds() * 1000
+    minutes = int(total_ms // 60000)
+    seconds = int((total_ms % 60000) // 1000)
+    milliseconds = int(total_ms % 1000)
+
+    return f"{minutes:02}:{seconds:02}.{milliseconds:03}"
+
+
 driver = session.get_driver('VER')
 driver_laps = session.laps.pick_drivers('VER')
 conditions = session.weather_data
 
 print(driver['DriverNumber']) #driver number
 
-print(driver_laps['LapTime'])
+
+for i in driver_laps['LapTime']:
+    print(format_time_delta(i))
 
 
-#print(driver_laps['LapTime_str'].min()) #fastest lap of the session
+print(driver_laps['LapTime_str'].min()) #fastest lap of the session
 
 print("minimum air temp for the session: ", conditions['AirTemp'].min(), "C") #minimum air temp of session
 print("maximum air temp for the session: ", conditions['AirTemp'].max(), "C")
